3e9e039906b038022e5d670383665cfe44244149
Wei Liu <wliu@highground.com>
2017-10-08 21:26:54 -0500

    Ui

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,5 +1,25 @@
-angular.module('MainCtrl', []).controller('MainController', function($scope) {
+angular.module('MainCtrl', []).controller('MainController', function($scope, $http) {
 
-	$scope.tagline = 'To the moon and back!';	
+	$scope.model = {
+		userName: ''
+	};
 
-});
+	function loginService(username) {
+        return $http({
+                  url: '/login',
+                  method: 'POST',
+                  data: {
+                      username: username
+                  }
+              }).then(function (response) {
+              	return response.data;
+				});
+	}
+
+	$scope.login = function () {
+        loginService($scope.model.userName)
+			.then(function(user){
+				$scope.currentUser = user;
+			});
+	};
+})
