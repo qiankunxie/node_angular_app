5c04ad3a559f71640ada30342469ccf244992ad5
Qiankun Xie <xieqiankun@wustl.edu>
2017-10-08 19:22:54 -0500

    update

--- app/models/User.js
+++ app/models/User.js
@@ -3,6 +3,7 @@ var mongoose = require('mongoose');
 
 // define our nerd model
 // module.exports allows us to pass this to other files when it is called
-module.exports = mongoose.model('Nerd', {
-	name : {type : String, default: ''}
+module.exports = mongoose.model('User', {
+	name : {type : String, default: ''},
+	coins: {type: Number, default: 1000}
 });
--- app/routes.js
+++ app/routes.js
@@ -1,13 +1,31 @@
 module.exports = function(app) {
 
-	// server routes ===========================================================
-	// handle things like api calls
-	// authentication routes
-
-	// frontend routes =========================================================
-	// route to handle all angular requests
 	app.get('*', function(req, res) {
 		res.sendfile('./public/index.html');
 	});
 
+	app.post('/login', function (req, res) {
+		var username = req.body.username,
+			User = require("./models/User");
+		if (!username) {
+			return res.send('You need send your user name to server');
+		}
+		User.findOne({name: username}, function (error, user) {
+			if (error) {
+				return res.send("")
+			}
+			console.log(user);
+			if (!user) {
+				//register user
+				user = new User({
+					name: username
+				});
+				user.save(function (error) {
+					res.send("dsadsadsa");
+				});
+			} else {
+				res.send("dsadsadsa");
+			}
+		});
+	});
 };
