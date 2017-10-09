efea4d6ec59f56760f109300059aa515a1f5a420
Qiankun Xie <xieqiankun@wustl.edu>
2017-10-09 15:52:27 -0500

    auction directive

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,1 @@
-angular.module('sampleApp', ['ngRoute', 'appRoutes', 'MainCtrl', 'NerdService', 'GeekService', 'UserDirective', 'InventoryDirective'])
+angular.module('sampleApp', ['ngRoute', 'appRoutes', 'MainCtrl', 'NerdService', 'GeekService', 'UserDirective', 'InventoryDirective', 'AuctionDirective'])
