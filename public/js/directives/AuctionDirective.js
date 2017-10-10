53c0dc0601f159afe58221383b916e9695b3639f
Qiankun Xie <xieqiankun@wustl.edu>
2017-10-09 21:30:38 -0500

    add auction count down

--- public/js/directives/AuctionDirective.js
+++ public/js/directives/AuctionDirective.js
@@ -1,5 +1,5 @@
 angular.module('AuctionDirective', [])
-.directive("auctionWidget", function ($http, $window, MainService) {
+.directive("auctionWidget", function ($http, $window, MainService, $timeout) {
     return {
         restrict: 'E',
         scope: {
@@ -7,18 +7,36 @@ angular.module('AuctionDirective', [])
         },
         templateUrl: 'views/auction.html',
         link: function (scope, element, attr) {
+            // timer
+            scope.counter = 0;
+            var mytimeout = null,
+                onTimeout = function() {
+                    if(scope.counter ===  0) {
+                        $timeout.cancel(mytimeout);
+                        return;
+                    }
+                    scope.counter--;
+                    mytimeout = $timeout(onTimeout, 1000);
+                },
+                startTimer = function() {
+                    mytimeout = $timeout(onTimeout, 1000);
+                },
+                initAuction = function (auction) {
+                    scope.auction = auction;
+                    scope.counter = Math.floor((auction.finishdate - Date.now()) / 1000);
+                    $timeout.cancel(mytimeout);
+                    startTimer();
+                }
             // init the web socket
             MainService.getAuction().then(function (auction) {
                 if (auction) {
-                    scope.auction = auction;
+                    initAuction(auction);
                 }
             });
             var socket = $window.io();
             socket.on('update-auction', function(auction){
-                scope.auction = auction;
-                console.log("update-auction", auction);
+                initAuction(auction);
             });
-            console.log(scope);
         }
     };
 });
